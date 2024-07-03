c96f84b6b324bb92ebe36c51d43a411240593a50
yabuki-nako <lesterlusung1414@gmail.com>
2024-07-03 13:35:10 +0800

    asdasd

--- src/assets/pages/contact.jsx
+++ src/assets/pages/contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const contact = () => {
   return (
     <div className='border-b border-neutral-900 pb-10'>
-      <h1 className='my-10 text-center text-4xl'>Get in touch with</h1>
+      <h1 className='my-10 text-center text-4xl'>Get in touch</h1>
       <div className='text-center tracking-tighter'>
         <p className='my-4'>Calamba, Laguna</p>
         <p className='my-4'>+(63) 995 715 9190</p>
